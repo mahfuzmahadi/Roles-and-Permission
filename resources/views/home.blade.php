a1b4e17650fe195174fe63acdb7d54a6238b25a0
Your Name <mahfuzurmahadi@gmail.com>
2023-04-08 13:31:01 +0600

    crud

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 
 @section('content')
@@ -6,7 +7,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
